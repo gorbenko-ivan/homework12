200267deb777c5552f3707d49ce6ca1479cdf5d0
gorbenko-ivan <Puzo05@mail.ru>
2024-09-09 10:50:43 +0300

    сделал домашку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author bookOne = new Author("Александр", "Пушкин");
+        Author bookTwo = new Author("Николай", "Гоголь");
+        Book one = new Book("Евгений Онегин", bookOne, 1833);
+        Book two = new Book("Тарас Бульба", bookTwo, 1835);
+        System.out.println(one.getAuthor().getFirstname() + " " + one.getAuthor().getLastname() + " " + one.getYearOfPublication());
+        System.out.println(two.getAuthor().getFirstname() + " " + two.getAuthor().getLastname() + " " + two.getYearOfPublication());
+        one.setYearOfPublication(1900);
+        System.out.println(one.getAuthor().getFirstname() + " " + one.getAuthor().getLastname() + " " + one.getYearOfPublication());
     }
 }
